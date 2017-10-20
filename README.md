cf377c299b7261f5c8841efbdf85813e62d9c375
Stagiaire AFIB <stage@afib.fr>
2017-10-20 15:35:32 +0200

    Add liscences, fix article-creator

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# FormationArticleCreator
+# Licence
+
+Ce(tte) œuvre est mise à disposition selon les termes de la [Licence Creative Commons Attribution - Pas d’Utilisation Commerciale - Partage dans les Mêmes Conditions 3.0 France.](https://creativecommons.org/licenses/by-nc-sa/3.0/fr/)
--- article-creator.php
+++ article-creator.php
@@ -12,9 +12,10 @@ $res = $req->fetch(PDO::FETCH_OBJ);
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="css/article-creator.css" />
     <link rel="stylesheet" href="template/main.css" />
+    <link rel="stylesheet" hred="css/scrollbar.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-<body style="padding-top:70px;">
+<body style="padding-top:70px;overflow-x:hidden;">
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -29,79 +30,79 @@ $res = $req->fetch(PDO::FETCH_OBJ);
     <div id="preview" style="display:none;" class="ac-container">
 
     </div>
-    <div class="row" style="padding: 8px;" id="edit-page">
-        <div class="col-md-2">
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <div class="toolbox-button">
-                        <!-- On utilise le data-toogle de bootstrap pour collapse le panel. -->
-                        <a data-toggle="collapse" href="#column-list"><i class="glyphicon glyphicon-plus" onclick="collapse"></i></a>
-                    </div>
-                    <h3 class="panel-title">Mise en page</h3>
+    <div class="sidebar-custom scrollbar" id="style-2">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <div class="toolbox-button">
+                    <!-- On utilise le data-toogle de bootstrap pour collapse le panel. -->
+                    <a data-toggle="collapse" href="#column-list"><i class="glyphicon glyphicon-plus" onclick="collapse"></i></a>
                 </div>
-                <div class="panel-body collapse in" id="column-list">
-                    <ul class="list-group"> <!-- On défini chaque colonne avec un data-column (C'est un attribut personnalisé) qui défini, séparé par une virgule, les colonnes qui seront ajouté -->
-                        <li class="list-group-item column" data-column="12"><span class="badge"><i class="glyphicon glyphicon-plus"></i></span> 1 colonne</li>
-                        <li class="list-group-item column" data-column="6,6"><span class="badge"><i class="glyphicon glyphicon-plus"></i></span> 2 colonne</li>
-                        <li class="list-group-item column" data-column="7,5"><span class="badge"><i class="glyphicon glyphicon-plus"></i></span> 2 colonne grand gauche</li>
-                        <li class="list-group-item column" data-column="5,7"><span class="badge"><i class="glyphicon glyphicon-plus"></i></span> 2 colonne grand droit</li>
-                        <li class="list-group-item column" data-column="8,4"><span class="badge"><i class="glyphicon glyphicon-plus"></i></span> 2 colonne petit gauche</li>
-                        <li class="list-group-item column" data-column="4,8"><span class="badge"><i class="glyphicon glyphicon-plus"></i></span> 2 colonne petit droit</li>
-                        <li class="list-group-item column" data-column="4,4,4"><span class="badge"><i class="glyphicon glyphicon-plus"></i></span> 3 colonne</li>
-                        <li class="list-group-item column" data-column="3,3,3,3"><span class="badge"><i class="glyphicon glyphicon-plus"></i></span> 4 colonne</li>
-                    </ul>
-                </div>
-                <div class="panel-footer">
-                    <span class="help-block">
-                        Effectuer votre mise en page avec vos colonnes.
-                    </span>
-                </div>
+                <h3 class="panel-title">Mise en page</h3>
             </div>
-            <br>
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <div class="toolbox-button">
-                        <a data-toggle="collapse" href="#module-list"><i class="glyphicon glyphicon-plus" onclick="collapse"></i></a>
-                    </div>
-                    <h3 class="panel-title">Modules</h3>
-                </div>
-                <div class="panel-body collapse in" id="module-list">
-                    <ul class="list-group">
-                        <!-- On défini chaque colonne avec un data-module afin de pouvoir le récupérer dans Javascript -->
-                        <li class="list-group-item module" data-module="text"><span class="badge"><i class="fa fa-file-text"></i></span> Paragraphe</li>
-                        <li class="list-group-item module" data-module="image"><span class="badge"><i class="fa fa-picture-o"></i></span> Image</li>
-                        <li class="list-group-item module" data-module="youtube"><span class="badge"><i class="fa fa-youtube-play"></i></span> Youtube Vidéo</li>
-                        <li class="list-group-item module" data-module="vimeo"><span class="badge"><i class="fa fa-vimeo"></i></span> Viméo Vidéo</li>
-                        <li class="list-group-item module" data-module="map"><span class="badge"><i class="fa fa-map"></i></span> Map</li>
-                    </ul>
-                </div>
-                <div class="panel-footer">
-                    <span class="help-block">
-                        Ajoutez des modules à votre page.
-                    </span>
-                </div>
+            <div class="panel-body collapse in" id="column-list">
+                <ul class="list-group"> <!-- On défini chaque colonne avec un data-column (C'est un attribut personnalisé) qui défini, séparé par une virgule, les colonnes qui seront ajouté -->
+                    <li class="list-group-item column" data-column="12"><span class="badge"><i class="glyphicon glyphicon-plus"></i></span> 1 colonne</li>
+                    <li class="list-group-item column" data-column="6,6"><span class="badge"><i class="glyphicon glyphicon-plus"></i></span> 2 colonne</li>
+                    <li class="list-group-item column" data-column="7,5"><span class="badge"><i class="glyphicon glyphicon-plus"></i></span> 2 colonne grand gauche</li>
+                    <li class="list-group-item column" data-column="5,7"><span class="badge"><i class="glyphicon glyphicon-plus"></i></span> 2 colonne grand droit</li>
+                    <li class="list-group-item column" data-column="8,4"><span class="badge"><i class="glyphicon glyphicon-plus"></i></span> 2 colonne petit gauche</li>
+                    <li class="list-group-item column" data-column="4,8"><span class="badge"><i class="glyphicon glyphicon-plus"></i></span> 2 colonne petit droit</li>
+                    <li class="list-group-item column" data-column="4,4,4"><span class="badge"><i class="glyphicon glyphicon-plus"></i></span> 3 colonne</li>
+                    <li class="list-group-item column" data-column="3,3,3,3"><span class="badge"><i class="glyphicon glyphicon-plus"></i></span> 4 colonne</li>
+                </ul>
             </div>
-            <br>
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <div class="toolbox-button">
-                        <a data-toggle="collapse" href="#action-list"><i class="glyphicon glyphicon-plus" onclick="collapse"></i></a>
-                    </div>
-                    <h3 class="panel-title">Actions</h3>
+            <div class="panel-footer">
+                <span class="help-block">
+                    Effectuer votre mise en page avec vos colonnes.
+                </span>
+            </div>
+        </div>
+        <br>
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <div class="toolbox-button">
+                    <a data-toggle="collapse" href="#module-list"><i class="glyphicon glyphicon-plus" onclick="collapse"></i></a>
                 </div>
-                <div class="panel-body collapse in" id="action-list">
-                    <button class="btn btn-primary btn-block" id="button-preview">Prévisualiser</button>
-                    <button class="btn btn-success btn-block" id="button-publish">Publier</button>
-                    <div class="alert alert-info" role="alert" id="success-publish" style="display:none;">
-                        <b>Succès ! </b> La publication est réussi !
-                    </div>
-                    <div class="alert alert-danger" role="alert" id="error-publish" style="display:none;">
-                        <b>Erreur : </b> Impossible de générer la page. Veuillez rééssayer.
-                    </div>
+                <h3 class="panel-title">Modules</h3>
+            </div>
+            <div class="panel-body collapse in" id="module-list">
+                <ul class="list-group">
+                    <!-- On défini chaque colonne avec un data-module afin de pouvoir le récupérer dans Javascript -->
+                    <li class="list-group-item module" data-module="text"><span class="badge"><i class="fa fa-file-text"></i></span> Paragraphe</li>
+                    <li class="list-group-item module" data-module="image"><span class="badge"><i class="fa fa-picture-o"></i></span> Image</li>
+                    <li class="list-group-item module" data-module="youtube"><span class="badge"><i class="fa fa-youtube-play"></i></span> Youtube Vidéo</li>
+                    <li class="list-group-item module" data-module="vimeo"><span class="badge"><i class="fa fa-vimeo"></i></span> Viméo Vidéo</li>
+                    <li class="list-group-item module" data-module="map"><span class="badge"><i class="fa fa-map"></i></span> Map</li>
+                </ul>
+            </div>
+            <div class="panel-footer">
+                <span class="help-block">
+                    Ajoutez des modules à votre page.
+                </span>
+            </div>
+        </div>
+        <br>
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <div class="toolbox-button">
+                    <a data-toggle="collapse" href="#action-list"><i class="glyphicon glyphicon-plus" onclick="collapse"></i></a>
+                </div>
+                <h3 class="panel-title">Actions</h3>
+            </div>
+            <div class="panel-body collapse in" id="action-list">
+                <button class="btn btn-primary btn-block" id="button-preview">Prévisualiser</button>
+                <button class="btn btn-success btn-block" id="button-publish">Publier</button>
+                <div class="alert alert-info" role="alert" id="success-publish" style="display:none;">
+                    <b>Succès ! </b> La publication est réussi !
+                </div>
+                <div class="alert alert-danger" role="alert" id="error-publish" style="display:none;">
+                    <b>Erreur : </b> Impossible de générer la page. Veuillez rééssayer.
                 </div>
             </div>
         </div>
-        <div class="col-md-10">
+    </div>
+    <div class="row" style="padding: 8px;padding-left:258px;" id="edit-page">
+        <div class="col-md-12">
             <!-- La div qui contient le contenu de notre article -->
             <div class="canvas-article" id="drop-area">
                 <div class="label-canvas label-drop">Contenu de la page</div>
